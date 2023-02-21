1f92a3cab1df5b73d66ca00b67c1f94125fff784
na3doit <108612577+na3doit@users.noreply.github.com>
2023-02-21 17:48:01 +0000

    Deploying to gh-pages from @ na3doit/goit-js-hw-11@4c04170c047f6903cf6a55f3228cdadc0f732786 🚀

--- index.1fa4f5ec.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".search-form");document.querySelector("input"),document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=index.1fa4f5ec.js.map
--- index.7b434bd8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".search-form");document.querySelector("input"),document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault()}))}();
-//# sourceMappingURL=index.7b434bd8.js.map
